353f5ea65d5b6ed49bb8e5c3a352e6364afb1a17
Kathryn Ragudos <kragudos19@gmail.com>
2017-10-03 22:19:48 -0700

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # , :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  class << self
+    def serialize_from_session(key,salt)                              
+      record = to_adapter.get(key.to_s)
+      record if record && record.authenticatable_salt == salt
+    end
+  end
 end
