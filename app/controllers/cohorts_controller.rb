2d7371a2e2344820775740a0f810ed6028d8636f
Kathryn Ragudos <kragudos19@gmail.com>
2017-10-07 15:03:03 -0700

    authenicate user before creating cohort action

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,5 +1,6 @@
 class CohortsController < ApplicationController
   before_action :set_cohort, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /cohorts
   # GET /cohorts.json
