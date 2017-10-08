87b4096df9afeec959157f9e36e8395f7b60672c
Kathryn Ragudos <kragudos19@gmail.com>
2017-10-08 12:55:57 -0700

    member_signed_in? method

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,9 +9,11 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
-    @user = User.find_by(email: params[:user][:email])
-    @user.authenticate(params[:user][:password])
-    redirect_to user_root_path
+    if member_signed_in?
+      redirect_to user_root_path
+    else
+      puts "else"
+    end
     super
   end
 
